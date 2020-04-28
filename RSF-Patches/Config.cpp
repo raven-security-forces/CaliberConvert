154733c89f92ab38fb1bdfd901eda0f4778f2f2c
NeroTheHero111 <nerothehero111@gmail.com>
2020-04-28 16:39:34 +0200

    Flags and Patches
    - RSF-Patches:
    
    Initial Release: Added 18 haha funny Patches
    
    - Gear-Reskins
    
    Updated: RSF Flag
    
    Added: RSF Banner,
    Sørland Flag and Banner

---
+++ RSF-Patches/Config.cpp
@@ -0,0 +1,161 @@
+class CfgPatches {
+
+	class RSF_Patches
+	{
+		units[] = {};
+		weapons[] = {};
+		requiredAddons[] = {};
+		version = "1.0";
+		author[]= {"RSF"};
+		authorUrl = "https://units.arma3.com/unit/raven-security-forces";
+	};
+
+};
+
+class CfgUnitInsignia
+{
+	class RSF_1SecP
+	{
+		displayName = "[RSF] 1 Section";
+		author = "NeroTheHero111";
+		texture = "\RSF-Patches\Data\1SecP.paa";
+		textureVehicle = "";
+	};
+	
+	class RSF_2SecP
+	{
+		displayName = "[RSF] 2 Section";
+		author = "NeroTheHero111";
+		texture = "\RSF-Patches\Data\2SecP.paa";
+		textureVehicle = "";
+	};	
+	
+	class RSF_3SecP
+	{
+		displayName = "[RSF] 3 Section";
+		author = "NeroTheHero111";
+		texture = "\RSF-Patches\Data\3SecP.paa";
+		textureVehicle = "";
+	};	
+	
+	class RSF_4SecP
+	{
+		displayName = "[RSF] 4 Section";
+		author = "NeroTheHero111";
+		texture = "\RSF-Patches\Data\4SecP.paa";
+		textureVehicle = "";
+	};	
+	
+	class RSF_5SecP
+	{
+		displayName = "[RSF] 5 Section";
+		author = "NeroTheHero111";
+		texture = "\RSF-Patches\Data\5SecP.paa";
+		textureVehicle = "";
+	};	
+	
+	class RSF_ACE_Report
+	{
+		displayName = "[RSF] ACE Report";
+		author = "NeroTheHero111";
+		texture = "\RSF-Patches\Data\ACE_Report.paa";
+		textureVehicle = "";
+	};	
+	
+	class RSF_Armour
+	{
+		displayName = "[RSF] Armour";
+		author = "NeroTheHero111";
+		texture = "\RSF-Patches\Data\ArmourP.paa";
+		textureVehicle = "";
+	};	
+	
+	class RSF_Astolfo
+	{
+		displayName = "[RSF] Astolfo";
+		author = "NeroTheHero111";
+		texture = "\RSF-Patches\Data\Astolfo.paa";
+		textureVehicle = "";
+	};	
+	
+	class RSF_Coomer
+	{
+		displayName = "[RSF] Coomer";
+		author = "NeroTheHero111";
+		texture = "\RSF-Patches\Data\Coomer.paa";
+		textureVehicle = "";
+	};	
+	
+	class RSF_Delta
+	{
+		displayName = "[RSF] Spec-Ops";
+		author = "NeroTheHero111";
+		texture = "\RSF-Patches\Data\Delta.paa";
+		textureVehicle = "";
+	};	
+	
+	class RSF_DeltaP
+	{
+		displayName = "[RSF] Delta";
+		author = "NeroTheHero111";
+		texture = "\RSF-Patches\Data\DeltaP.paa";
+		textureVehicle = "";
+	};	
+	
+	class RSF_JACP
+	{
+		displayName = "[RSF] JAC";
+		author = "NeroTheHero111";
+		texture = "\RSF-Patches\Data\JACP.paa";
+		textureVehicle = "";
+	};
+	
+	class RSF_Ricardo
+	{
+		displayName = "[RSF] Ricardo";
+		author = "NeroTheHero111";
+		texture = "\RSF-Patches\Data\Ricardo.paa";
+		textureVehicle = "";
+	};	
+	
+	class RSF_Patch
+	{
+		displayName = "[RSF] RSF Patch";
+		author = "NeroTheHero111";
+		texture = "\RSF-Patches\Data\RSF.paa";
+		textureVehicle = "";
+	};	
+	
+	class RSF_Tom_Patch
+	{
+		displayName = "[RSF] RSF Patch (Haunted)";
+		author = "NeroTheHero111";
+		texture = "\RSF-Patches\Data\RSF_Tom_Patch.paa";
+		textureVehicle = "";
+	};		
+	
+	class RSF_Trollface
+	{
+		displayName = "[RSF] Trollface";
+		author = "NeroTheHero111";
+		texture = "\RSF-Patches\Data\Trollface.paa";
+		textureVehicle = "";
+	};
+	
+	class RSF_Warcrimes_1
+	{
+		displayName = "[RSF] I have commited serveral Warcrimes";
+		author = "NeroTheHero111";
+		texture = "\RSF-Patches\Data\Warcrimes.paa";
+		textureVehicle = "";
+	};
+	
+	class RSF_Warcrimes_2
+	{
+		displayName = "[RSF] At this point I only feel Recoil";
+		author = "NeroTheHero111";
+		texture = "\RSF-Patches\Data\Warcrimes2.paa";
+		textureVehicle = "";
+	};	
+	
+};
