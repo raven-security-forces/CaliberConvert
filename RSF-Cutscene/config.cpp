687d624812a98fea4d0483abcba5b5de70527145
NeroTheHero111 <nerothehero111@gmail.com>
2020-09-14 12:10:58 +0200

    removed FIR dependency

--- RSF-Cutscene/config.cpp
+++ RSF-Cutscene/config.cpp
@@ -5,7 +5,7 @@ class CfgPatches
 		units[] = {};
 		weapons[] = {};
 		requiredVersion = 0.2;
-		requiredAddons[] = {"FIR_AWS_Cutscene_F","A3_Map_Stratis","A3_Map_Altis","A3_Map_Stratis_Scenes_F","A3_Map_Altis_Scenes_F","A3_Map_VR_Scenes_F","A3_Map_Malden_Scenes_F","A3_Missions_F_Orange","A3_Map_Tanoa_Scenes_F"};
+		requiredAddons[] = {"A3_Map_Stratis","A3_Map_Altis","A3_Map_Stratis_Scenes_F","A3_Map_Altis_Scenes_F","A3_Map_VR_Scenes_F","A3_Map_Malden_Scenes_F","A3_Missions_F_Orange","A3_Map_Tanoa_Scenes_F"};
 		
 	};
 };
@@ -54,9 +54,7 @@ class CfgMusic
 class CfgAddons 
 {
 	class PreloadAddons 
-	{
-		delete FIR_AWS_Cutscene_F;
-	
+	{	
 		class RSF_Cutscene_F
 		{
 			list[] = {
